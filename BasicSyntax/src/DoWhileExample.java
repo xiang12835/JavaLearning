7b429b8b4119e41fc0828cd9ea1e5b85a4e95e42
yu.xiang <yu.xiang@alibaba-inc.com>
2019-07-07 14:53:09 +0800

    add_while

---
+++ BasicSyntax/src/DoWhileExample.java
@@ -0,0 +1,8 @@
+public class DoWhileExample {
+    public static void main(String[] args) {
+
+        do {
+            System.out.println("do-while 会执行一次");
+        } while (false); // 以;结束
+    }
+}
