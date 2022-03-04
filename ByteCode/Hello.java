bdaab77c4cbf2d8a867a29a385c4367547d8e71f
Shane Yoo <yu.xiang@alibaba-inc.com>
2022-03-04 11:41:47 +0800

    add_bytecode

---
+++ ByteCode/Hello.java
@@ -0,0 +1,15 @@
+/* Java 字节码
+
+ 编译：javac Hello.java
+
+ 查看字节码：javap -c Hello
+
+ 查看字节码详细信息：javap -c -verbose Hello
+
+* */
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
